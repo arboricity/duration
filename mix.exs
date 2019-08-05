1f4a6581659986f0792797718e531ea4fac60ec6
Xavier Barbosa <clint.northwood@gmail.com>
2019-08-05 14:34:43 +0200

    license

--- mix.exs
+++ mix.exs
@@ -7,27 +7,13 @@ defmodule Duration.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
-
-      # Docs
       name: "Duration",
       source_url: "https://lab.errorist.xyz/elixir/duration",
       homepage_url: "http://elixir.errorist.io/duration",
-      docs: [
-        main: "Duration", # The main page in the docs
-        # logo: "path/to/logo.png",
-        extras: [
-          "guides/advanced/timex.md",
-        ],
-        groups_for_extras: [
-          # "Introduction": Path.wildcard("guides/introduction/*.md"),
-          "Advanced": Path.wildcard("guides/advanced/*.md")
-        ]
-      ],
-      # extras: [
-      #   "guides/introduction/foo.md",
-
-
+      docs: docs()
     ]
   end
 
@@ -42,10 +28,33 @@ defmodule Duration.MixProject do
   defp deps do
     [
       {:nimble_parsec, "~> 0.5.0", only: [:dev, :test]},
-      # {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      # {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
-      # {:timex, "~> 3.6", only: [:dev, :test], runtime: false}
-      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Duration is a tiny package which allows to manipulate ISO 8601:2004 durations."
+  end
+
+  defp package do
+    [
+      name: "duration",
+      licenses: ["BSD-3-Clause"],
+      # links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
+    ]
+  end
+
+  defp docs do
+    docs: [
+      main: "Duration", # The main page in the docs
+      # logo: "path/to/logo.png",
+      extras: [
+        "guides/advanced/timex.md",
+      ],
+      groups_for_extras: [
+        # "Introduction": Path.wildcard("guides/introduction/*.md"),
+        "Advanced": Path.wildcard("guides/advanced/*.md")
+      ]
     ]
   end
 end
